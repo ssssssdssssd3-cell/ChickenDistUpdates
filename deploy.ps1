2b5758910afea8282dc3addc86b1483946089773
Tamim <tamim@example.com>
2026-08-04 02:20:22 +0300

    deploy: v2.0.274 - 2026-08-04 02:20

--- deploy.ps1
+++ deploy.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = "Stop"
 # ────────────────────────────────────────────────────────────
 # ⚙️ Settings
 # ────────────────────────────────────────────────────────────
-$VERSION   = "2.0.273"
+$VERSION   = "2.0.274"
 $CHANGELOG = Get-Content -Path (Join-Path $PSScriptRoot "changelog.txt") -Raw -Encoding UTF8
 $UPDATE_URL = "https://raw.githubusercontent.com/ssssssdssssd3-cell/ChickenDistUpdates/main/ChickenDist.bin"
 
