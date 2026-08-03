879534945d6e042ef18cbdfbdf224a4542658125
Tamim <tamim@example.com>
2026-08-03 12:25:26 +0300

    deploy: v2.0.251 - 2026-08-03 12:25

--- Core/UpdateManager.cs
+++ Core/UpdateManager.cs
@@ -12,7 +12,7 @@ namespace ChickenDist.Core
     public static class UpdateManager
     {
         // الإصدار الحالي للبرنامج
-        public const string CurrentVersion = "2.0.250";
+        public const string CurrentVersion = "2.0.251";
         
         // رابط ملف التحديث النصي على GitHub
         private const string UpdateUrl = "https://raw.githubusercontent.com/ssssssdssssd3-cell/ChickenDistUpdates/main/update.txt";
